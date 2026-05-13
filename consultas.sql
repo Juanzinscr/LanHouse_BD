6e3e0c2a3807450a33e2ef8844da44617cae0e54
Juanzinscr <145047570+Juanzinscr@users.noreply.github.com>
2026-05-13 00:10:27 -0300

    Adicionado consultas SQL para clientes, sessões, produtos e vendas.

---
+++ consultas.sql
@@ -0,0 +1,18 @@
+-- Listar clientes
+SELECT * FROM cliente;
+
+-- Sessões com nome do cliente
+SELECT c.nome, s.inicio, s.fim
+FROM sessoes s
+JOIN cliente c
+ON s.id_cliente = c.id_cliente;
+
+-- Produtos em estoque
+SELECT nome, qtd_estoque
+FROM produto;
+
+-- Vendas realizadas
+SELECT v.id_venda, c.nome, v.valor_total
+FROM venda v
+JOIN cliente c
+ON v.id_cliente = c.id_cliente;
