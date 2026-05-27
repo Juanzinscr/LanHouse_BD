e0f4aa2142fff9fe4f71ebdaf3c8328907be39c1
Juanzinscr <145047570+Juanzinscr@users.noreply.github.com>
2026-05-26 23:01:50 -0300

    Atualizar data_cadastro para clientes
    Atualizei o 'data_cadastro' para vários clientes com novas datas.

--- consultas-e-insert_semana2.sql
+++ consultas-e-insert_semana2.sql
@@ -1,6 +1,78 @@
 USE lan_house;
 
 -- =====================================
+-- DATA_CADASTRO
+-- =====================================
+
+UPDATE cliente
+SET data_cadastro = '2026-01-05'
+WHERE id_cliente = 1;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-08'
+WHERE id_cliente = 2;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-10'
+WHERE id_cliente = 3;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-12'
+WHERE id_cliente = 4;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-15'
+WHERE id_cliente = 5;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-18'
+WHERE id_cliente = 6;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-20'
+WHERE id_cliente = 7;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-23'
+WHERE id_cliente = 8;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-25'
+WHERE id_cliente = 9;
+
+UPDATE cliente
+SET data_cadastro = '2026-01-28'
+WHERE id_cliente = 10;
+
+UPDATE cliente
+SET data_cadastro = '2026-02-01'
+WHERE id_cliente = 11;
+
+UPDATE cliente
+SET data_cadastro = '2026-02-03'
+WHERE id_cliente = 12;
+
+UPDATE cliente
+SET data_cadastro = '2026-02-06'
+WHERE id_cliente = 13;
+
+UPDATE cliente
+SET data_cadastro = '2026-02-10'
+WHERE id_cliente = 14;
+
+UPDATE cliente
+SET data_cadastro = '2026-02-14'
+WHERE id_cliente = 15;
+
+UPDATE cliente
+SET data_cadastro = '2026-02-18'
+WHERE id_cliente = 16;
+
+UPDATE cliente
+SET data_cadastro = '2026-02-20'
+WHERE id_cliente = 17;
+
+-- =====================================
 -- CONSULTA_1
 -- =====================================
 
